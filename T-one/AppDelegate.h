eb1820dd98f27c46878cf35a4c2d2a4fd811beb3
Cheney <Cheneykids@Cheney.local>
2015-09-13 20:33:48 +0800

    Initial Commit

---
+++ T-one/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  T-one
+//
+//  Created by Cheney on 9/13/15.
+//  Copyright (c) 2015 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ T-one/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  T-one
+//
+//  Created by Cheney on 9/13/15.
+//  Copyright (c) 2015 Cheney. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
